c323c53e93fe6c12ccec72bd25b8e7de71e60b0f
Felipe Barros <felipe.b4rros@gmail.com>
2022-02-16 15:07:07 -0300

    initial commit

---
+++ python/owm_to_table.py
@@ -0,0 +1,28 @@
+import csv
+from datetime import datetime
+
+import requests
+
+# url = 'https://api.openweathermap.org/data/2.5/onecall?lat=-27.36649&lon=-55.89411&mode=metric&exclude={current,minutely,daily,alerts}&lang=es&appid=32a020e350d198b03a3acfbcdd355310'
+url = 'https://api.openweathermap.org/data/2.5/onecall?lat=-26.22701&lon=-54.01472&mode=metric&exclude={current,minutely,daily,alerts}&lang=es&appid=32a020e350d198b03a3acfbcdd355310'
+
+resp = requests.get(url=url)  # , params=params)
+data = resp.json()  # Check the JSON Response Content documentation below
+
+# data.keys()
+
+# data.get("hourly")
+# len(data.get("hourly"))
+for hora in data.get("hourly"):
+    to_convert_dt = hora.get('dt')
+    converted_dt = datetime.utcfromtimestamp(hora.get('dt')).strftime('%Y-%m-%d %H:%M:%S')
+    to_convert_temp = hora.get('temp')
+    converted_temp = round(to_convert_temp - 273.15, 2)
+    hora.update(dt=converted_dt, temp=converted_temp)
+
+keys = data.get("hourly")[0].keys()
+
+with open('weather.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(data.get("hourly"))
