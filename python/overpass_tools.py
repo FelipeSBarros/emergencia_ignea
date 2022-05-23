311713d2f9f591df367c3d6db9946773b2793c11
Felipe Barros <felipe.b4rros@gmail.com>
2022-05-23 12:52:20 -0300

    change overpass functions to be used on apply; add queries to clean_misiones_data

--- python/overpass_tools.py
+++ python/overpass_tools.py
@@ -1,116 +1,152 @@
+import time
+import time
 import overpy
+from overpy.exception import OverpassTooManyRequests
 
 overpass_api = overpy.Overpass()
 
-def has_indigenous_population(overpass_api, buffer_distance, Longitud, Latitud):
-    # Latitud = -27.094
-    # Longitud = -54.9722
+
+def has_indigenous_population(row, overpass_api, buffer_distance):
+    # row.geometry.y = -27.094
+    # row.geometry.x = -54.9722
     # buffer_distance = 10000
 
     RiskFactor = 0
-    query = f"node(around:{buffer_distance},{Latitud},{Longitud})[place=hamlet][name~'Comunidad Aborigen']; out meta;"
-    indigenous_population = overpass_api.query(query)
+    query = f"node(around:{buffer_distance},{row.geometry.y},{row.geometry.x})[place=hamlet][name~'Comunidad Aborigen']; out meta;"
+    try:
+        indigenous_population = overpass_api.query(query)
+        time.sleep(30)
+    except OverpassTooManyRequests:
+        time.sleep(30)
+        indigenous_population = overpass_api.query(query)
 
-    if (
-        indigenous_population.nodes
-        or indigenous_population.ways
-    ):
+    if indigenous_population.nodes or indigenous_population.ways:
         RiskFactor = 10
 
     return RiskFactor
 
-def has_gas_station(overpass_api, buffer_distance, Longitud, Latitud):
+
+def has_gas_station(row, overpass_api, buffer_distance):
     # buffer_distance = 500
-    # Latitud = -26.9912
-    # Longitud = -54.4885
+    # row.geometry.y = -26.9912
+    # row.geometry.x = -54.4885
     RiskFactor = 0
 
-    query = f"node(around:{buffer_distance},{Latitud},{Longitud})[amenity=fuel];out meta;"
-    gas_stations = overpass_api.query(query)
+    query = f"node(around:{buffer_distance},{row.geometry.y},{row.geometry.x})[amenity=fuel];out meta;"
+    try:
+        gas_stations = overpass_api.query(query)
+        time.sleep(30)
+    except OverpassTooManyRequests:
+        time.sleep(30)
+        gas_stations = overpass_api.query(query)
 
-    if (
-            gas_stations.nodes
-            or gas_stations.ways
-    ):
-        RiskFactor = 9
-
-    return RiskFactor
-
-def has_inflamable_gas(overpass_api, buffer_distance, Longitud, Latitud):
-    # Latitud = -27.44457;
-    # Longitud = -55.87458;
-    RiskFactor = 0
-
-    query = f"node(around:{buffer_distance},{Latitud},{Longitud})[shop=gas];out meta;"
-    gas = overpass_api.query(query)
-
-    if (
-            gas.nodes
-            or gas.ways
-    ):
+    if gas_stations.nodes or gas_stations.ways:
         RiskFactor = 9
 
     return RiskFactor
 
 
-def gas_supply(overpass_api, buffer_distance, Longitud, Latitud):
-    # Latitud = -27.44457
-    # Longitud = -55.87458
+def has_inflamable_gas(row, overpass_api, buffer_distance):
+    # row.geometry.y = -27.44457;
+    # row.geometry.x = -55.87458;
     RiskFactor = 0
 
-    query = f'way(around:{buffer_distance},{Latitud},{Longitud})[man_made=storage_tank][substation=gas];out meta;'
-    gas_suppply = overpass_api.query(query)
-    if (
-            gas_suppply.nodes or
-            gas_suppply.ways
-    ):
+    query = f"node(around:{buffer_distance},{row.geometry.y},{row.geometry.x})[shop=gas];out meta;"
+    try:
+        gas = overpass_api.query(query)
+        time.sleep(30)
+    except OverpassTooManyRequests:
+        time.sleep(30)
+        gas = overpass_api.query(query)
+
+    if gas.nodes or gas.ways:
+        RiskFactor = 9
+
+    return RiskFactor
+
+
+def gas_supply(row, overpass_api, buffer_distance):
+    # row.geometry.y = -27.44457
+    # row.geometry.x = -55.87458
+    RiskFactor = 0
+
+    query = f"way(around:{buffer_distance},{row.geometry.y},{row.geometry.x})[man_made=storage_tank][substation=gas];out meta;"
+    try:
+        gas_suppply = overpass_api.query(query)
+        time.sleep(30)
+    except OverpassTooManyRequests:
+        time.sleep(30)
+        gas_suppply = overpass_api.query(query)
+
+    if gas_suppply.nodes or gas_suppply.ways:
         RiskFactor = 9
     return RiskFactor
 
 
-def gas_supply(overpass_api, buffer_distance, Longitud, Latitud):
-    # Latitud = -27.42830
-    # Longitud = -55.89126
+def has_schools(row, overpass_api, buffer_distance):
+    # row.geometry.y = -27.42830
+    # row.geometry.x = -55.89126
+    RiskFactor = 0
+    query = f"node(around:{buffer_distance},{row.geometry.y},{row.geometry.x})[amenity=school];out meta;"
+    try:
+        schools = overpass_api.query(query)
+        time.sleep(30)
+    except OverpassTooManyRequests:
+        time.sleep(30)
+        schools = overpass_api.query(query)
 
-    query = f'node(around:{buffer_distance},{Latitud},{Longitud})[amenity=school];out meta;'
-    schools = overpass_api.query(query)
-
-    if (
-            schools.nodes or schools.ways
-    ):
+    if schools.nodes or schools.ways:
         RiskFactor = 8
     return RiskFactor
 
 
-def timber_supply(overpass_api, buffer_distance, Longitud, Latitud):
-    # Latitud = -27.47436
-    # Longitud = -55.15989
+def timber_supply(row, overpass_api, buffer_distance):
+    # row.geometry.y = -27.47436
+    # row.geometry.x = -55.15989
     RiskFactor = 0
-    query = f'node(around:{buffer_distance},{Latitud},{Longitud})[craft=sawmill];out meta;'
-    timber = overpass_api.query(query)
-    if( timber.nodes or timber.ways ):
+    query = f"node(around:{buffer_distance},{row.geometry.y},{row.geometry.x})[craft=sawmill];out meta;"
+    try:
+        timber = overpass_api.query(query)
+        time.sleep(30)
+    except OverpassTooManyRequests:
+        time.sleep(30)
+        timber = overpass_api.query(query)
 
-        RiskFactor = 7;
+    if timber.nodes or timber.ways:
+
+        RiskFactor = 7
     return RiskFactor
 
-def power_line(overpass_api, buffer_distance, Longitud, Latitud):
-    # Latitud = -27.47611;
-    # Longitud = -55.15734;
 
-    query = f'way(around:{buffer_distance},{Latitud},{Longitud})[power=line];out meta;'
-    power_line = overpass_api.query(query)
-    if (
-        power_line.nodes or
-        power_line.ways
-    ):
-        RiskFactor = 5;
+def power_line(row, overpass_api, buffer_distance):
+    # row.geometry.y = -27.47611;
+    # row.geometry.x = -55.15734;
+    RiskFactor = 0
+    query = f"way(around:{buffer_distance},{row.geometry.y},{row.geometry.x})[power=line];out meta;"
+    try:
+        power_line = overpass_api.query(query)
+        time.sleep(30)
+    except OverpassTooManyRequests:
+        time.sleep(30)
+        power_line = overpass_api.query(query)
+
+    if power_line.nodes or power_line.ways:
+        RiskFactor = 5
     return RiskFactor
 
-def urban_areas(overpass_api, buffer_distance, Longitud, Latitud):
-    # Latitud = -26.69454
-    # Longitud = -54.20307
-    query = f'relation(around:{buffer_distance},{Latitud},{Longitud})[landuse=residential];out meta;'
-    urban_areas = overpass_api.query(query)
-    if (urban_areas.relations or urban_areas.nodes or urban_areas.ways):
-        RiskFactor = 5;
+
+def urban_areas(row, overpass_api, buffer_distance):
+    # row.geometry.y = -26.69454
+    # row.geometry.x = -54.20307
+    RiskFactor = 0
+    query = f"relation(around:{buffer_distance},{row.geometry.y},{row.geometry.x})[landuse=residential];out meta;"
+    try:
+        urban_areas = overpass_api.query(query)
+        time.sleep(30)
+    except OverpassTooManyRequests:
+        time.sleep(30)
+        urban_areas = overpass_api.query(query)
+
+    if urban_areas.relations or urban_areas.nodes or urban_areas.ways:
+        RiskFactor = 5
     return RiskFactor
--- python/request_data.py
+++ python/request_data.py
@@ -3,6 +3,21 @@ import io
 import logging
 import sys
 
+import pandas
+
+from python.overpass_tools import (
+    has_indigenous_population,
+    has_gas_station,
+    has_inflamable_gas,
+    gas_supply,
+    gas_supply,
+    timber_supply,
+    power_line,
+    urban_areas,
+    has_schools,
+)
+import overpy
+
 import geopandas as gpd
 import pandas as pd
 import requests
@@ -14,6 +29,7 @@ urls = [
     "https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_South_America_24h.csv",
     "https://firms.modaps.eosdis.nasa.gov/data/active_fire/noaa-20-viirs-c2/csv/J1_VIIRS_C2_South_America_24h.csv",
 ]
+overpass_api = overpy.Overpass()
 
 
 def get_data(url):
@@ -59,6 +75,33 @@ def clean_data(incendios_df):
         sys.exit()
 
     else:
+        logger.info("acessing RiskFactor form OSM data")
+        df = pd.DataFrame()
+        df.index = incendios_cleaned["index"]
+        df["Aborigen"] = incendios_cleaned.apply(
+            has_indigenous_population, args=(overpass_api, 10000), axis=1
+        )
+        df["EstacionServicio"] = incendios_cleaned.apply(
+            has_gas_station, args=(overpass_api, 10000), axis=1
+        )
+        df["gas"] = incendios_cleaned.apply(
+            has_inflamable_gas, args=(overpass_api, 10000), axis=1
+        )
+        df["Escuelas"] = incendios_cleaned.apply(
+            has_schools, args=(overpass_api, 10000), axis=1
+        )
+        df["Asserradero"] = incendios_cleaned.apply(
+            timber_supply, args=(overpass_api, 10000), axis=1
+        )
+        df["Energia"] = incendios_cleaned.apply(
+            power_line, args=(overpass_api, 10000), axis=1
+        )
+        df["AreaUrb"] = incendios_cleaned.apply(
+            urban_areas, args=(overpass_api, 10000), axis=1
+        )
+        df["riskfactor"] = None
+        df["riskfactor"] = df.max(axis=1)
+        incendios_cleaned = incendios_cleaned.join(df)
         return incendios_cleaned
 
 
